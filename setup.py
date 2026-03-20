05a5b5d015febde3d4b4974a7d6a0daddcb7e245
kushagra <kushagramodanwal@gmail.com>
2026-03-20 19:50:58 +0530

    git

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Kushagra Modanwal",
+    author_email="kushagramodanwal@gmail.com",
+    packages=find_packages(),   
+    install_requires=[]
+) 
+
